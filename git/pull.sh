57d7c182a8b6fe491b480e56f5ede324ee6ecbfa
Joko Putranto <akun.joko.putranto@gmail.com>
2014-10-18 00:32:28 +0700

    git/pull: echo

--- git/pull.sh
+++ git/pull.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+echo "Update git ..."
 for x in `ls`
 do
  if [ -d "$x" ]
  then
+  echo "- Folder $x:"
   cd $x
   for y in `ls`
   do
    if [ -d "$y/.git" ]
    then
+    echo "  + Projects: $y"
     cd $y
     git pull
     cd ..
