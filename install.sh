f8aa71b52d100e1a80ba6914b9819195cb636fec
Joko Putranto <akun.joko.putranto@gmail.com>
2014-10-27 11:01:53 +0700

    install fix; htdocs fix

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-if ( whoami != root )
-then echo "Jalankan dengan pengguna root"
-else
+echo "Jalankan dengan pengguna root"
 
- chmod +x /home/jp/three-musketeerz/centos-scripts/git/pull.sh
- chmod +x /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh
- chmod +x /home/jp/three-musketeerz/centos-scripts/rsync/rsync-htdocs.sh
+chmod +x /home/jp/three-musketeerz/centos-scripts/git/pull.sh
+chmod +x /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh
+chmod +x /home/jp/three-musketeerz/centos-scripts/rsync/htdocs.sh
 
- ln -sf /home/jp/three-musketeerz/centos-scripts/git/pull.sh /usr/bin/pull
- ln -sf /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh /usr/bin/run-pull
- ln -sf /home/jp/three-musketeerz/centos-scripts/rsync/rsync-htdocs.sh /usr/sbin/rsync-htdocs
+ln -sf /home/jp/three-musketeerz/centos-scripts/git/pull.sh /usr/bin/pull
+ln -sf /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh /usr/bin/run-pull
+ln -sf /home/jp/three-musketeerz/centos-scripts/rsync/htdocs.sh /usr/sbin/rsync-htdocs
 
-fi
--- rsync/htdocs.sh
+++ rsync/htdocs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 rsync -avz --delete \
-  /home/jp/idx-repo/kbli/www /var/www/html/kbli
+  /home/jp/idx-repo/kbli/www/ /var/www/html/kbli/
 rsync -avz --delete \
-  /home/jp/idx-repo/simple-profile/www /var/www/html/sp
+  /home/jp/idx-repo/simple-profile/www/ /var/www/html/sp/
 rsync -avz --delete \
-  /home/jp/idx-repo/registered-company-profile/www /var/www/html/rcp
+  /home/jp/idx-repo/registered-company-profile/www/ /var/www/html/rcp/
 rsync -avz --delete \
-  /home/jp/yiisoft/yii/framework /var/www/html/framework
+  /home/jp/yiisoft/yii/framework/ /var/www/html/framework/
