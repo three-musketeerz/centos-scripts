f0f26bf21625522536abe34977d1204664066740
Kohar Jidan <kohar.jidan@gmail.com>
2014-11-11 09:43:03 +0700

    fix git/run-pull.sh

--- git/run-pull.sh
+++ git/run-pull.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ssh-agent sh -c \
-  'ssh-add < /dev/null && sh ~/three-musketeerz/centos-scripts/git/pull.sh'
+  'ssh-add < /dev/null && sh $PWD/three-musketeerz/centos-scripts/git/pull.sh'
