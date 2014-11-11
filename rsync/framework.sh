457ae203c6886321de9df851ce7f21231e04034e
Joko Putranto <akun.joko.putranto@gmail.com>
2014-11-11 13:19:11 +0700

    rsync: htdocs fix & framework

---
+++ rsync/framework.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/kbli/framework/
+rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/simple-profile/framework/
+rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/registered-company-profile/framework/
+
--- rsync/htdocs.sh
+++ rsync/htdocs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -avz /home/jp/idx-repo/kbli/www/ /var/www/html/kbli/
-rsync -avz /home/jp/idx-repo/simple-profile/www/ /var/www/html/sp/
-rsync -avz /home/jp/idx-repo/registered-company-profile/www/ /var/www/html/rcp/
-rsync -avz /home/jp/yiisoft/yii/framework/ /var/www/html/framework/
+rsync -avz --delete /home/jp/idx-repo/kbli/www/ /var/www/html/kbli/
+rsync -avz --delete /home/jp/idx-repo/simple-profile/www/ /var/www/html/sp/
+rsync -avz --delete /home/jp/idx-repo/registered-company-profile/www/ /var/www/html/rcp/
+rsync -avz --delete /home/jp/yiisoft/yii/framework/ /var/www/html/framework/
