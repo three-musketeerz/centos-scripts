97a4df7053e9392536cf0144b82b2a5b5bf6d6ca
Joko Putranto <akun.joko.putranto@gmail.com>
2014-11-27 17:22:01 +0700

    minor fix : echo which projects updated

--- rsync/framework.sh
+++ rsync/framework.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+echo "KBLI"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/kbli/framework/
+
+echo "Simple Profile"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/simple-profile/framework/
+
+echo "Registered Company Profile"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/registered-company-profile/framework/
 
