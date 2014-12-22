aabea3377c15f87478fbb7afb935fb0d5a3f9340
Joko Putranto <akun.joko.putranto@gmail.com>
2014-12-22 16:53:25 +0700

    rsync: framework minor fix

--- rsync/framework.sh
+++ rsync/framework.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
+echo ""
 echo "KBLI"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/kbli/framework/
-
+echo ""
 echo "Simple Profile"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/simple-profile/framework/
-
+echo ""
 echo "Registered Company Profile"
 rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/registered-company-profile/framework/
 
