bd0a8cf50099a3deb3d1961d89104e734a290682
Joko Putranto <akun.joko.putranto@gmail.com>
2015-05-03 10:14:49 +0700

    yii framework sync to projects

--- rsync/framework.sh
+++ rsync/framework.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo ""
 echo "KBLI"
-rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/kbli/framework/
+rsync -avz --update --delete ~/yiisoft/yii/framework/ ~/idx-repo/kbli/framework/
 echo ""
 echo "Simple Profile"
-rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/simple-profile/framework/
+rsync -avz --update --delete ~/yiisoft/yii/framework/ ~/idx-repo/simple-profile/framework/
 echo ""
 echo "Registered Company Profile"
-rsync -avz --delete ~/yiisoft/yii/framework/ ~/idx-repo/registered-company-profile/framework/
+rsync -avz --update --delete ~/yiisoft/yii/framework/ ~/idx-repo/registered-company-profile/framework/
+echo ""
 
