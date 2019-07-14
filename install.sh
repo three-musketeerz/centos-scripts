98179c69ea45f86b14d980c62615bca8ca7ff3bf
JokoPW <akun.joko.putranto@gmail.com>
2019-07-14 18:53:20 +0000

    install di user

--- install.sh
+++ install.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
-echo "Jalankan dengan pengguna root"
+#echo "Jalankan dengan pengguna root"
 
-chmod +x /home/jp/three-musketeerz/centos-scripts/git/pull.sh
-chmod +x /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh
-chmod +x /home/jp/three-musketeerz/centos-scripts/rsync/htdocs.sh
-chmod +x /home/jp/three-musketeerz/centos-scripts/rsync/framework.sh
-chmod +x /home/jp/three-musketeerz/centos-scripts/git/update-yii.sh
+chmod +x git/pull.sh
+chmod +x git/run-pull.sh
+chmod +x rsync/htdocs.sh
+chmod +x rsync/framework.sh
+chmod +x git/update-yii.sh
 
-ln -sf /home/jp/three-musketeerz/centos-scripts/git/pull.sh \
-	/usr/bin/pull
-ln -sf /home/jp/three-musketeerz/centos-scripts/git/run-pull.sh \
-	/usr/bin/run-pull
-ln -sf /home/jp/three-musketeerz/centos-scripts/rsync/htdocs.sh \
-	/usr/sbin/rsync-htdocs
-ln -sf /home/jp/three-musketeerz/centos-scripts/rsync/framework.sh \
-	/usr/bin/rsync-fw
-ln -sf /home/jp/three-musketeerz/centos-scripts/git/update-yii.sh \
-        /usr/bin/update-yii
+ln -sf ~/three-musketeerz/centos-scripts/git/pull.sh ~/bin/pull
+ln -sf ~/three-musketeerz/centos-scripts/git/run-pull.sh ~/bin/run-pull
+ln -sf ~/three-musketeerz/centos-scripts/rsync/htdocs.sh ~/bin/rsync-htdocs
+ln -sf ~/three-musketeerz/centos-scripts/rsync/framework.sh ~/bin/rsync-fw
+ln -sf ~/three-musketeerz/centos-scripts/git/update-yii.sh ~/bin/update-yii
